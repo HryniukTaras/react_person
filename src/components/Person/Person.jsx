01f62e398653fefbc76c09d1669cbcc29ea43776
Hryniuk_Taras <gritaras484@gmail.com>
2024-04-24 10:22:16 +0300

    edited <section> in Person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,12 +7,10 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
+
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried ? (
-        <p className="Person__partner">{married}</p>
-      ) : (
-        <p className="Person__partner">{married}</p>
-      )}
+
+      <p className="Person__partner">{married}</p>
     </section>
   );
 };
